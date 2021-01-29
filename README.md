e5a9a281f9dee431ca9d1970e4cf8809f69545d5
Gerardo Silva <gsilva.razo@gmail.com>
2021-01-28 20:54:29 -0600

    Create scene, add court and player animated

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Proyecto Final - Gráficas Computacionales</h1>
+<p>Como proyecto para la clase de gráficas computacionales, de manera individual, se desarrollará utilizando la librería Three.js una escena con una cancha de basketball texturas, iluminación, sombras y un personaje animado botando una pelota.</p>
+<br>
+<h3>Assets</h3>
+<ul>
+  <li>Cancha de basketball</li>
+  <li>Personaje</li>
+</ul>
+<br>
--- app.js
+++ app.js
@@ -1,85 +1,205 @@
-var vertexShaderText = [
-  "precision mediump float;",
-  "",
-  "attribute vec2 vertPosition;",
-  "attribute vec3 vertColor;",
-  "varying vec3 fragColor;",
-  "",
-  "void main()",
-  "{",
-  "  fragColor = vertColor;",
-  "  gl_Position = vec4(vertPosition, 0.0, 1.0);",
-  "}",
-].join("\n");
+// Import three.js library
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.125.1/build/three.module.js";
 
-var fragmentShaderText = [
-  "precision mediump float;",
-  "",
-  "varying vec3 fragColor;",
-  "void main()",
-  "{",
-  "  gl_FragColor = vec4(fragColor, 1.0);",
-  "}",
-].join("\n");
+// Import module to load .fbx models and animations
+import { FBXLoader } from "https://cdn.jsdelivr.net/npm/three@0.125.1/examples/jsm/loaders/FBXLoader.js";
+// Import module to load models and textures
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.125.1/examples/jsm/loaders/GLTFLoader.js";
+// Import module to add mouse actions
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.125.1/examples/jsm/controls/OrbitControls.js";
 
-var init = function () {
-  console.log("Hello World");
-
-  var canvas = document.getElementById("myCanvas");
-  var gl = canvas.getContext("webgl");
-
-  if (!gl) {
-    console.log("WebGL not supported, falling back on experimental-webgl");
-    gl = canvas.getContext("experimental-webgl");
+class LoadProyect {
+  constructor() {
+    this._Initialize();
   }
 
-  if (!gl) {
-    alert("Your browser does not support WebGL");
-  }
+  _Initialize() {
+    this._threejs = new THREE.WebGLRenderer({
+      antialias: true,
+    });
+    this._threejs.shadowMap.enabled = true;
+    this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
+    this._threejs.setPixelRatio(window.devicePixelRatio);
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
 
-  gl.clearColor(1.0, 1.0, 1.0, 1.0); //
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    document.body.appendChild(this._threejs.domElement);
 
-  // Create shaders
-  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-  gl.shaderSource(vertexShader, vertexShaderText);
-  gl.shaderSource(fragmentShader, fragmentShaderText);
-
-  gl.compileShader(vertexShader);
-  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    console.error(
-      "ERROR compiling vertex shader!",
-      gl.getShaderInfoLog(vertexShader)
+    window.addEventListener(
+      "resize",
+      () => {
+        this._OnWindowResize();
+      },
+      false
     );
-    return;
-  }
 
-  gl.compileShader(fragmentShader);
-  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    console.error(
-      "ERROR compiling fragment shader!",
-      gl.getShaderInfoLog(fragmentShader)
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1.0;
+    const far = 1000.0;
+    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this._camera.position.set(75, 20, 0);
+
+    this._scene = new THREE.Scene();
+
+    let light = new THREE.DirectionalLight(0xffffff, 1.0);
+    light.position.set(20, 100, 10);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true;
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.near = 0.5;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.left = 100;
+    light.shadow.camera.right = -100;
+    light.shadow.camera.top = 100;
+    light.shadow.camera.bottom = -100;
+    this._scene.add(light);
+
+    light = new THREE.AmbientLight(0xffffff, 4.0);
+    this._scene.add(light);
+
+    const controls = new OrbitControls(this._camera, this._threejs.domElement);
+    controls.target.set(0, 20, 0);
+    controls.update();
+
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      "./resources/posx.jpg",
+      "./resources/negx.jpg",
+      "./resources/posy.jpg",
+      "./resources/negy.jpg",
+      "./resources/posz.jpg",
+      "./resources/negz.jpg",
+    ]);
+    this._scene.background = texture;
+
+    const plane = new THREE.Mesh(
+      new THREE.PlaneGeometry(100, 100, 10, 10),
+      new THREE.MeshStandardMaterial({
+        color: 0x101090,
+      })
     );
-    return;
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    this._scene.add(plane);
+
+    this._previousRAF = null;
+
+    // this._LoadAnimatedModel();
+    // this._LoadAnimatedModelAndPlay(
+    //     './resources/dancer/', 'girl.fbx', 'dance.fbx', new THREE.Vector3(0, -1.5, 5));
+    // this._LoadAnimatedModelAndPlay(
+    //     './resources/dancer/', 'dancer.fbx', 'Silly Dancing.fbx', new THREE.Vector3(12, 0, -10));
+    // this._LoadAnimatedModelAndPlay(
+    //     './resources/dancer/', 'dancer.fbx', 'Silly Dancing.fbx', new THREE.Vector3(-12, 0, -10));
+
+    this._LoadModel();
+    this._LoadAnimatedModel();
+    this._RAF();
   }
 
-  var program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.error("ERROR linking program!", gl.getProgramInfoLog(program));
-    return;
-  }
-  gl.validateProgram(program);
-  if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-    console.error("ERROR validating program!", gl.getProgramInfoLog(program));
-    return;
+  _LoadAnimatedModel() {
+    const loader = new FBXLoader();
+    loader.setPath("./resources/");
+    loader.load("player.fbx", (fbx) => {
+      fbx.scale.setScalar(0.1);
+      fbx.traverse((c) => {
+        c.castShadow = true;
+      });
+
+      // const params = {
+      //   target: fbx,
+      //   camera: this._camera,
+      // };
+      // this._controls = new BasicCharacterControls(params);
+
+      const anim = new FBXLoader();
+      anim.setPath("./resources/");
+      anim.load("dribble.fbx", (anim) => {
+        // const m = new THREE.AnimationMixer(fbx);
+        // this._mixers.push(m);
+        this._mixer = new THREE.AnimationMixer(fbx);
+        // const idle = m.clipAction(anim.animations[0]);
+        const idle = this._mixer.clipAction(anim.animations[0]);
+        idle.play();
+      });
+      this._scene.add(fbx);
+    });
   }
 
-  // Main render loop
-  gl.useProgram(program);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
-};
+  _LoadAnimatedModelAndPlay(path, modelFile, animFile, offset) {
+    const loader = new FBXLoader();
+    loader.setPath(path);
+    loader.load(modelFile, (fbx) => {
+      fbx.scale.setScalar(0.1);
+      fbx.traverse((c) => {
+        c.castShadow = true;
+      });
+      fbx.position.copy(offset);
+
+      const anim = new FBXLoader();
+      anim.setPath(path);
+      anim.load(animFile, (anim) => {
+        const m = new THREE.AnimationMixer(fbx);
+        this._mixers.push(m);
+        const idle = m.clipAction(anim.animations[0]);
+        idle.play();
+      });
+      this._scene.add(fbx);
+    });
+  }
+
+  _LoadModel() {
+    const loader = new GLTFLoader();
+    loader.load("./resources/court/BasketBallCourt.gltf", (gltf) => {
+      gltf.scene.traverse((c) => {
+        c.castShadow = true;
+      });
+      this._scene.add(gltf.scene);
+    });
+  }
+
+  _OnWindowResize() {
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._camera.updateProjectionMatrix();
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  _RAF() {
+    requestAnimationFrame((t) => {
+      if (this._previousRAF === null) {
+        this._previousRAF = t;
+      }
+
+      this._RAF();
+
+      this._threejs.render(this._scene, this._camera);
+      this._Step(t - this._previousRAF);
+      this._previousRAF = t;
+    });
+  }
+
+  _Step(timeElapsed) {
+    if (this._mixer) {
+      this._mixer.update(timeElapsed * 0.001);
+    }
+    //   const timeElapsedS = timeElapsed * 0.001;
+    //   if (this._mixers) {
+    //     this._mixers.map((m) => m.update(timeElapsedS));
+    //   }
+
+    //   if (this._controls) {
+    //     this._controls.Update(timeElapsedS);
+    //   }
+  }
+}
+
+let _APP = null;
+
+window.addEventListener("DOMContentLoaded", () => {
+  _APP = new LoadProyect();
+});
